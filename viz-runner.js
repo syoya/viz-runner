b59e528662e03d7ba43e2f8d6628f20c3abc3cc8
Daisuke Mino <daisuke.mino@gmail.com>
2015-11-02 13:37:58 +0900

    Update viz-runner.js

--- viz-runner.js
+++ viz-runner.js
@@ -1,6 +1,6 @@
 (function() {
   var script = document.createElement('script');
-  script.setAttribute('src', 'http://mdaines.github.io/viz.js/bower_components/viz.js/viz.js');
+  script.setAttribute('src', 'https://rawgit.com/mdaines/viz.js/gh-pages/bower_components/viz.js/viz.js');
   script.onload = function() {
     [].forEach.call(document.querySelectorAll('script[type="text/viz"]'), function(s) {
       var code = s.innerText || s.textContent;
